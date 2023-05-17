c21bad0fe24ed984b101e8ca07b29cc148cd501a
AgneBalc <agne.martinaityte@gmail.com>
2023-05-17 14:55:56 +0300

    validation for signup created

--- src/components/pages/signup/Signup.jsx
+++ src/components/pages/signup/Signup.jsx
@@ -1,9 +1,43 @@
+import * as yup from 'yup';
 import Button from "../../UI/button/Button";
 import Form from "../../UI/form/Form";
 import Input from "../../UI/input/Input"
 import StyledSignup from "./StyledSignup";
 
 const Signup = () => {
+
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .trim()
+      .required('Required field!'),
+    username: yup
+      .string()
+      .trim()
+      .min(3, 'Username is too short! Must be at least 3 characters')
+      .max(20, 'Username is too long! Must be 15 characters or less')
+      .required('Required field!'),
+    picture: yup
+      .string()
+      .trim()
+      .matches(
+        /\bhttps?:\/\/\S+?\.(?:png|jpe?g|gif|bmp)\b/,
+        'Invalid URL!'
+      ),
+    password: yup
+      .string()
+      .trim()
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d).{8,}$/,
+        "Password must contain at least eight characters, including at least 1 letter and 1 number."
+      )
+      .required('Required field!'),
+    passwordConfirm: yup
+      .mixed()
+      .oneOf([yup.ref('password')], "Passwords don't match!")
+      .required('Required field!')
+  });
+
   return (
     <StyledSignup>
       <h1>Sign Up</h1>
@@ -29,7 +63,6 @@ const Signup = () => {
           type='password'
           id='password'
         />
-        <p className="passwordMsg">Password must contain at least eight characters, including at least 1 letter and 1 number.</p>
         <Input
           label='Re-enter password'
           type='password'
