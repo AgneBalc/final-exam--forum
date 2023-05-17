c7f6ada6d56b6ba1e15a2ac62f694b714cc5d627
AgneBalc <agne.martinaityte@gmail.com>
2023-05-17 16:14:21 +0300

    get and add users functionality created

--- src/contexts/users-context.js
+++ src/contexts/users-context.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const UsersContext = createContext();
 
@@ -13,11 +13,40 @@ const USERS_ACTIONS = {
   LOGIN: 'login_user',
   LOGOUT: 'logout_user',
   ADD: 'add_new_user',
-}
+};
 
+const usersReducer = (state, action) => {
+  switch (action.type) {
+    case USERS_ACTIONS.GET:
+      return { ...state, users: action.data };
+    case USERS_ACTIONS.ADD:
+      fetch('http://localhost:8080/users', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(action.user)
+      });
+      return {
+        ...state,
+        isLoggedIn: true,
+        loggedInUser: action.user,
+        users: [...state.users, action.users]
+      }
+    default:
+      return state;
+  }
+}
 
 const UsersProvider = ({ children }) => {
   const [users, dispatchUsers] = useReducer(usersReducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/users')
+      .then(res => res.json())
+      .then(data => dispatchUsers({
+        type: USERS_ACTIONS.GET,
+        data,
+      }))
+  }, []);
 
   return (
     <UsersContext.Provider
