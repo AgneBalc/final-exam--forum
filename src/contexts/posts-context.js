d1af21376b4da366beb0656c6f73eccdaad5e852
AgneBalc <agne.martinaityte@gmail.com>
2023-05-18 12:36:44 +0300

    get all posts function created

--- src/contexts/posts-context.js
+++ src/contexts/posts-context.js
@@ -1,9 +1,31 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const PostsContext = createContext();
 
+const POSTS_ACTIONS = {
+  GET: 'posts',
+}
+
+const postsReducer = (state, action) => {
+  switch (action.type) {
+    case POSTS_ACTIONS.GET:
+      return action.data;
+    default:
+      return state;
+  }
+}
+
 const PostsProvider = ({ children }) => {
   const [posts, dispatchPosts] = useReducer(postsReducer, []);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/posts')
+      .then(res => res.json())
+      .then(data => dispatchPosts({
+        type: POSTS_ACTIONS.GET,
+        data,
+      }))
+  }, []);
 
   return (
     <PostsContext.Provider
