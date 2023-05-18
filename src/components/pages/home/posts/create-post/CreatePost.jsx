2c9cc827f6f271fed164960d609162cce02fed56
AgneBalc <agne.martinaityte@gmail.com>
2023-05-18 15:17:42 +0300

    createPost component created and styles added

---
+++ src/components/pages/home/posts/create-post/CreatePost.jsx
@@ -0,0 +1,12 @@
+import StyledCreatePost from "./StyledCreatePost";
+
+const CreatePost = () => {
+  return (
+    <StyledCreatePost>
+      <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png" alt="" />
+      <input type="text" placeholder="Create Post" />
+    </StyledCreatePost>
+  );
+}
+
+export default CreatePost;
--- src/components/pages/home/posts/posts-container/PostsContainer.jsx
+++ src/components/pages/home/posts/posts-container/PostsContainer.jsx
@@ -2,12 +2,14 @@ import { useContext } from "react";
 import StyledPostsContainer from "./StyledPostsContainer";
 import PostsContext from "../../../../../contexts/posts-context";
 import Post from "../post/Post";
+import CreatePost from "../create-post/CreatePost";
 
 const PostsContainer = () => {
   const { posts } = useContext(PostsContext);
 
   return (
     <StyledPostsContainer>
+      <CreatePost />
       {posts.map(post =>
         <Post
           key={post.id}
