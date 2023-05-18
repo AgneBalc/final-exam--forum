81f8011650be78651852104d7ffa747f2cb383a6
AgneBalc <agne.martinaityte@gmail.com>
2023-05-18 15:41:47 +0300

    removed unnecessary component

--- src/components/pages/home/posts/posts-container/PostsContainer.jsx
+++ src/components/pages/home/posts/posts-container/PostsContainer.jsx
@@ -1,15 +1,20 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import StyledPostsContainer from "./StyledPostsContainer";
 import PostsContext from "../../../../../contexts/posts-context";
 import Post from "../post/Post";
-import CreatePost from "../create-post/CreatePost";
 
 const PostsContainer = () => {
   const { posts } = useContext(PostsContext);
 
   return (
     <StyledPostsContainer>
-      <CreatePost />
+      <div className="addPost">
+        <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png" alt="" />
+        <Link to='/add'>
+          <input type="text" placeholder="Create Post" />
+        </Link>
+      </div>
       {posts.map(post =>
         <Post
           key={post.id}
--- src/components/pages/home/posts/posts-container/StyledPostsContainer.jsx
+++ src/components/pages/home/posts/posts-container/StyledPostsContainer.jsx
@@ -6,6 +6,38 @@ const StyledPostsContainer = styled.section`
   flex-direction: column;
   gap: 1rem;
   margin: 2rem auto;
+  .addPost {
+    height: 58px;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    img {
+      height: 36px;
+    }
+    a {
+      width: 100%;
+      margin-right: 0.5rem;
+     input {
+        height: 38px;
+        border: 1px solid #edeff1;
+        background-color: #f6f7f8;
+        padding: 0 1rem;
+        width: 100%;
+        border-radius: 4px;
+        &:hover {
+          border: 1px solid #0665fc;
+        }
+        &::placeholder {
+          font-weight: 300;
+          color: #828282;
+          letter-spacing: 0.5px;
+        }
+      }
+    }
+  }
 `;
 
 export default StyledPostsContainer;
