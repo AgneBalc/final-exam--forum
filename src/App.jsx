284df691ae620cbc65ade3e6806874fa5a7e3ffc
AgneBalc <agne.martinaityte@gmail.com>
2023-05-18 10:37:55 +0300

    home component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Signup from './components/pages/signup/Signup';
 import Login from './components/pages/login/Login';
+import Home from './components/pages/home/Home';
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       {/* <Signup /> */}
-      <Login />
+      {/* <Login /> */}
     </>
   );
 }
---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import StyledHome from "./StyledHome";
+
+const Home = () => {
+  return (
+    <StyledHome>
+      <h1>Memes, jokes and questions about programming</h1>
+    </StyledHome>
+  );
+}
+
+export default Home;
