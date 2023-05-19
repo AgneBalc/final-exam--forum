fe754d959308cb1ed3ad6f8cbfcd752d3bc4d33a
AgneBalc <agne.martinaityte@gmail.com>
2023-05-19 10:35:52 +0300

    styles for tab buttons added

--- src/components/pages/create-post/StyledCreatePost.jsx
+++ src/components/pages/create-post/StyledCreatePost.jsx
@@ -1,17 +1,24 @@
 import styled from "styled-components";
 
 const StyledCreatePost = styled.main`
-  width: 514px;
+  width: 740px;
   .tabs {
+    padding: 0 15px;
+    height: 50px;
+    display: flex;
     button {
+      flex: 1;
       display: flex;
-      padding: 14px 0;
+      align-items: center;
+      justify-content: center;
+      gap: 0.5rem;
+      height: 100%;
       cursor: pointer;
-      font-weight: 700;
       color: #878a8c80;
       border-width: 0px 1px 1px 0px;
       border-bottom-color: #edeff1;
       border-right-color: #edeff1;
+      background-color: #fff;
       &:hover {
         background-color: #dae0e6;
       }
@@ -19,6 +26,14 @@ const StyledCreatePost = styled.main`
         color: #0079d3;
         border-width: 0px 1px 2px 0px;
         border-bottom-color: #0079d3;
+      }
+      span {
+        font-weight: 700;
+        font-size: 14px;
+      }
+      i {
+        font-size: 20px;
+        font-weight: 400;
       }
     }
   }
