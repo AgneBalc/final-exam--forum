23390fbce8e2258ddf9ccbe9455a6c5003286369
AgneBalc <agne.martinaityte@gmail.com>
2023-05-22 23:33:55 +0300

    posts filter by comments amount added

--- src/components/pages/home/posts/posts-container/PostsContainer.jsx
+++ src/components/pages/home/posts/posts-container/PostsContainer.jsx
@@ -5,24 +5,43 @@ import PostsContext from "../../../../../contexts/posts-context";
 import Post from "../post/Post";
 import UsersContext from "../../../../../contexts/users-context";
 import Filter from "../../../../features/filter/Filter";
+import CommentsContext from "../../../../../contexts/comments-context";
 
 const PostsContainer = () => {
   const { posts } = useContext(PostsContext);
+  const { comments } = useContext(CommentsContext);
   const { users: { isLoggedIn, loggedInUser } } = useContext(UsersContext);
   const [selectedFilter, setSelectedFilter] = useState(null);
 
-  if (selectedFilter === 'Best') {
-    posts.forEach(post => {
-      const totalLikes = post.likes.reduce((acc, curr) => acc + curr.likeValue, 0)
-      post.totalLikes = totalLikes;
-    });
-    posts.sort((a, b) => b.totalLikes - a.totalLikes);
-  }
+  const filterPostsFunc = () => {
+    switch (selectedFilter) {
+      case 'Best':
+        posts.forEach(post => {
+          const totalLikes = post.likes.reduce((acc, curr) => acc + curr.likeValue, 0)
+          post.totalLikes = totalLikes;
+        });
+        return posts.sort((a, b) => b.totalLikes - a.totalLikes);
+      case 'New':
+        posts.forEach(post => post.dateCreated = new Date(post.dateCreated))
+        return posts.sort((a, b) => b.dateCreated - a.dateCreated);
+      case 'Active':
+        posts.forEach(post => {
+          const totalComments = comments.filter(comment => comment.postId === post.id).length;
+          post.totalComments = totalComments;
+        });
+        return posts.filter(post => post.totalComments > 0)
+      case 'Inactive':
+        posts.forEach(post => {
+          const totalComments = comments.filter(comment => comment.postId === post.id).length;
+          post.totalComments = totalComments;
+        });
+        return posts.filter(post => post.totalComments === 0)
+      default:
+        return posts;
+    }
+  };
 
-  if (selectedFilter === 'New') {
-    posts.forEach(post => post.dateCreated = new Date(post.dateCreated))
-    posts.sort((a, b) => b.dateCreated - a.dateCreated);
-  }
+  const filterPosts = filterPostsFunc();
 
   return (
     <StyledPostsContainer>
@@ -38,10 +57,10 @@ const PostsContainer = () => {
         setSelectedFilter={setSelectedFilter}
         selectedFilter={selectedFilter}
       />
-      {posts?.length === 0 ? (
+      {filterPosts?.length === 0 ? (
         <p>Loading...</p>
       ) : (
-        posts.map(post =>
+        filterPosts.map(post =>
           <Post
             post={post}
             key={post.id}
